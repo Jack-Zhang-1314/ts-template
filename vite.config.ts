13515edff90965fbe960e8b63e1c5930e02a4b5e
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-05-20 14:36:17 +0800

    feat(init project): complete  husky check commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+/** @format */
+
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
+})
