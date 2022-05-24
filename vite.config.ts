a69ee867706b0b3acf5ed2f4617678ae0cefcf74
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-05-24 22:24:41 +0800

    feat: add .browserslistrc

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  define: {
+    //关闭选项api
+    __VUE_OPTIONS_API__: false,
+  },
   plugins: [
     vue(),
     Unocss({
